6d118c620705a0f343520bd6bbd30d32373c6ab7
Ejnar40 <Einar.lundqvist@gmail.com>
2024-05-21 19:00:34 +0200

    jfsdo

--- UnbornFetus/Assets/Code/PlayerCode/PlauerMovement.cs
+++ UnbornFetus/Assets/Code/PlayerCode/PlauerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlauerMovement : MonoBehaviour
 {
@@ -8,9 +10,14 @@ public class PlauerMovement : MonoBehaviour
     [Header(":3")]
 
     public float moveSpeed;
+    public float JumpForce;
+
+    public LayerMask GroundLayer;
+    private bool isGrounded = false;
+    public GameObject rayObject;
 
     public Transform orientation;
-
+    
     float horizontalInput;
     float verticalInput;
 
@@ -27,7 +34,20 @@ public class PlauerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        OwerInput();
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayObject.transform.position, Vector3.down, out hit, 0.5f, GroundLayer))
+        {
+            isGrounded = true;
+        }
+        else isGrounded = false;
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+            OwerInput();
     }
 
     private void FixedUpdate()
@@ -46,5 +66,9 @@ public class PlauerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
     }
 }
