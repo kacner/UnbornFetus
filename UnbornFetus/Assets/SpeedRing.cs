7cd353a986e54e0a70679e7e17d453b61b999c5d
Ejnar40 <Einar.lundqvist@gmail.com>
2024-05-30 22:01:51 +0200

    shawty got that fatty

--- UnbornFetus/Assets/SpeedRing.cs
+++ UnbornFetus/Assets/SpeedRing.cs
@@ -10,7 +10,7 @@ public class SpeedRing : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             rb = other.GetComponentInParent<Rigidbody>();
-            rb.velocity = new Vector3(rb.velocity.x * 1.3f, rb.velocity.y, rb.velocity.z * 1.3f);
+            rb.velocity = new Vector3(rb.velocity.x * 1.5f, rb.velocity.y, rb.velocity.z * 1.5f);
         }
         else
             Debug.Log(other + "dosent have a rb");
