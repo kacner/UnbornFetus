e98396b29e7ef59a0de93e1a5e3c90b9acc2bd4f
Ejnar40 <Einar.lundqvist@gmail.com>
2024-06-05 23:08:05 +0200

    PEDOMETER

---
+++ UnbornFetus/Assets/PedoMeter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PedoMeter : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    public float totalDistance;
+    public TextMeshProUGUI textMeshPro;
+
+    void Start()
+    {
+        lastPosition = new Vector3(transform.position.x, 0f, transform.position.z);
+        InvokeRepeating("DOSTUFF", 0.5f, 0.5f);
+    }
+
+    private void DOSTUFF()
+    {
+        Vector3 currentPosition = new Vector3(transform.position.x, 0f, transform.position.z);
+        float distanceThisFrame = Vector3.Distance(currentPosition, lastPosition);
+
+        totalDistance += distanceThisFrame;
+
+        lastPosition = currentPosition;
+
+        textMeshPro.text = totalDistance.ToString("F0");
+    }
+}
