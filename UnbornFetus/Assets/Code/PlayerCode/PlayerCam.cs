1da63a6b119d091dfad158e8bd9c29cd9f8bcafc
Ziondaman_SV <121035557+TheWhiteFoxTrot@users.noreply.github.com>
2024-06-14 12:54:08 +0200

    Cam fix, Skib fix, 90 uv fix

--- UnbornFetus/Assets/Code/PlayerCode/PlayerCam.cs
+++ UnbornFetus/Assets/Code/PlayerCode/PlayerCam.cs
@@ -7,139 +7,157 @@ using static UnityEngine.GraphicsBuffer;
 
 public class PlayerCam : MonoBehaviour
 {
-    public float snsX;
-    public float snsY;
+    [Header("Camera Sensitivity")]
+    public float snsX; // Sensitivity for the X-axis (horizontal)
+    public float snsY; // Sensitivity for the Y-axis (vertical)
 
-    public Transform orientation;
+    [Header("Speed Flair Settings")]
+    public Color startingColor; // Initial color for the speed lines
+    public float alpha = 0f; // Alpha value for the speed lines
 
-    float yRotation;
-    float xRotation;
+    [Header("Camera FOV Settings")]
+    public float baseFOV = 60f; // Base Field of View for the camera
+    public float maxFOVChange = 30f; // Maximum change in Field of View
+    public float shakeIntensity = 0.1f; // Intensity of the camera shake
+    public float shakeFrequency = 25f; // Frequency of the camera shake
 
-    public Transform cameraPos;
+    [Header("References Objects")]
+    public QuakeMovement quakemovment; // Reference to the QuakeMovement component
+    public Transform orientation; // Transform for the orientation
+    public Transform cameraPos; // Position of the camera
 
+    private Camera cam; // Camera component
+    private Vector3 originalPosition; // Original position of the camera
+    private float currentVelocity; // Current velocity of the player
 
-    private GameObject player;
-    private ParticleSystem particleSystem;
-    public Color startingColor;
-    public float alpha = 0f;
+    private float yRotation; // Rotation around the Y-axis
+    private float xRotation; // Rotation around the X-axis
 
-    public QuakeMovement quakemovment;
-    public float baseFOV = 60f;
-    public float maxFOVChange = 30f;
-    public float shakeIntensity = 0.1f;
-    public float shakeFrequency = 25f;
-    private Camera cam;
-    private Vector3 originalPosition;
-
-    private float currentVelocity;
+    private GameObject player; // Reference to the player object
+    private ParticleSystem particleSystem; // Particle system for speed lines
 
     void Start()
     {
+        // Hide Cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
-        {
-            quakemovment = player.GetComponent<QuakeMovement>();
-            if (quakemovment == null)
-            {
-                Debug.LogError("QuakeMovement component not found on player object.");
-            }
-        }
-        else
-            Debug.LogError("Player object not found with tag 'Player'.");
+        FailsSafeRunner(); // Initialize and validate references
 
-        cam = GetComponentInChildren<Camera>();
-        if (cam != null)
-            originalPosition = cam.transform.localPosition;
-        else
-            Debug.LogError("Camera component not found on this game object.");
-
-        GameObject speedlinesObject = GameObject.FindGameObjectWithTag("Speedlines");
-        if (speedlinesObject != null)
-        {
-            particleSystem = speedlinesObject.GetComponent<ParticleSystem>();
-            if (particleSystem == null)
-            {
-                Debug.LogError("ParticleSystem component not found on Speedlines object.");
-            }
-        }
-        else
-            Debug.LogError("Speedlines object not found with tag 'Speedlines'.");
-
+        // Set the initial color for the particle system
         Color particleColor = new Color(startingColor.r, startingColor.g, startingColor.b, alpha);
         var mainModule = particleSystem.main;
         mainModule.startColor = particleColor;
     }
+
     private void FixedUpdate()
     {
+        // Update the current velocity based on the player's rigidbody velocity
         currentVelocity = quakemovment.GetComponent<Rigidbody>().velocity.magnitude;
     }
 
     void Update()
     {
-        transform.position = cameraPos.position;
+        PlayerCamra(); // Update the camera position and rotation
 
+        // Camera shake and FOV adjustments when moving
+        if (currentVelocity > 0)
+        {
+            AdjustFOV(currentVelocity); // Adjust Field of View
+            ApplyCameraShake(currentVelocity); // Apply camera shake
+        }
+        else
+            cam.transform.localPosition = originalPosition; // Reset camera position when not moving
+        
+
+        Speedlines(currentVelocity); // Update speed lines effect
+    }
+
+    void PlayerCamra()
+    {
+        transform.position = cameraPos.position; // Follow player
+
+        // Get user input for mouse movement
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * snsX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * snsY;
 
+        // Update rotation based on mouse input
         yRotation += mouseX;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        // Apply rotation to the camera and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
 
+        // Toggle cursor visibility with Escape and Mouse0 keys
         if (Input.GetKey(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-
-        alpha = Mathf.Clamp(currentVelocity / 10f, 0, 1);
-
-        // Camera shake and FOV
-        if (currentVelocity > 0)
+        else if (Input.GetKey(KeyCode.Mouse0))
         {
-            AdjustFOV(currentVelocity);
-            ApplyCameraShake(currentVelocity);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
-        else
-        {
-            cam.transform.localPosition = originalPosition;
-        }
-
-        Speedlines(currentVelocity);
     }
 
     void AdjustFOV(float velocity)
     {
+        // Adjust the Field of View based on the current velocity
         float fovChange = Mathf.Clamp(velocity, 0, maxFOVChange);
         cam.fieldOfView = baseFOV + fovChange;
         Mathf.Lerp(baseFOV, 0, fovChange);
-
     }
 
     void ApplyCameraShake(float velocity)
     {
+        // Apply camera shake based on the current velocity
         float shakeAmount = Mathf.Sin(Time.time * shakeFrequency) * shakeIntensity * (velocity / 2);
         cam.transform.localPosition = originalPosition + Random.insideUnitSphere * shakeAmount;
     }
 
     void Speedlines(float velocity)
     {
-        float alpha = CalculateAlpha(velocity);
+        // Update the alpha value of the speed lines based on the current velocity
+        float alpha = 1f / (1f + Mathf.Exp(-0.2f * (velocity - 40f))); // Formel: f(x) = 1 / (1 + e^(-0.2 * (x - 40)))
 
+        // Set the color of the particle system
         Color particleColor = new Color(startingColor.r, startingColor.g, startingColor.b, alpha);
         var mainModule = particleSystem.main;
         mainModule.startColor = particleColor;
     }
 
-    float CalculateAlpha(float velocity)
+    void FailsSafeRunner()
     {
+        // Find the player object and get the QuakeMovement component
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            quakemovment = player.GetComponent<QuakeMovement>();
+            if (quakemovment == null)
+                Debug.LogError("QuakeMovement component not found on player object.");
+        }
+        else
+            Debug.LogError("Player object not found with tag 'Player'.");
 
-        //      Formel: f(x) = 1 / (1 + e^(-0.2 * (x - 40)))
-        return 1f / (1f + Mathf.Exp(-0.2f * (velocity - 40f)));
+        // Find the camera component
+        cam = GetComponentInChildren<Camera>();
+        if (cam != null)
+            originalPosition = cam.transform.localPosition;
+        else
+            Debug.LogError("Camera component not found on this game object.");
+
+        // Find the Speedlines object and get the ParticleSystem component
+        GameObject speedlinesObject = GameObject.FindGameObjectWithTag("Speedlines");
+        if (speedlinesObject != null)
+        {
+            particleSystem = speedlinesObject.GetComponent<ParticleSystem>();
+            if (particleSystem == null)
+                Debug.LogError("ParticleSystem component not found on Speedlines object.");
+        }
+        else
+            Debug.LogError("Speedlines object not found with tag 'Speedlines'.");
     }
 }
--- UnbornFetus/Assets/Code/SKibidiBrainrot.cs
+++ UnbornFetus/Assets/Code/SKibidiBrainrot.cs
@@ -6,108 +6,125 @@ using UnityEngine;
 
 public class SKibidiBrainrot : MonoBehaviour
 {
-    public float speed = 7f;
-    private float changeInterval = 0.7f;
+    [Header("Fetus Settings")]
+    public float speed = 7f; // Initial speed of the object
+    public float helpTimer; // Timer to track help
 
-    public bool hasGameStarted;
-    public bool brakeAway;
+    [Header("Triggers")]
+    public bool hasGameStarted; // Flag to check if the game has started
+    public bool brakeAway; // Flag to check if the object has broken away
 
-    private Rigidbody rb;
-    private HingeJoint joint;
-    private float velocity;
-    public GameObject UmbilicalParticlesystem;
-    public PointPointMovie pointpointmove;
+    [Header("References Objects")]
+    public GameObject HelpingHeand; // Reference to the helping hand object
+    public GameObject helpingText; // Reference to the helping text object
+    public GameObject UmbilicalParticlesystem; // Reference to the umbilical particle system
+    public PointPointMovie pointpointmove; // Reference to the PointPointMovie script
 
-    public float helpTimer;
-    public GameObject HelpingHeand;
-    public GameObject helpingText;
-    private bool canRedo = true;
+    private float changeInterval = 0.7f; // Interval for changing direction
+
+    private Rigidbody rb; // Rigidbody component of the object
+    private HingeJoint joint; // HingeJoint component of the object
+    private float velocity; // Current velocity of the object
+
+    private bool canRedo = true; // Flag to check if help can be redone
 
     void Start()
     {
+        // Getting componets form References Objects
         joint = GetComponent<HingeJoint>(); 
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); 
+
+        Time.timeScale = 0.5f; // Slow down the game time
+
+        // Set up repeated method calls
         InvokeRepeating("ChangeDirection", 0f, changeInterval);
-        UmbilicalParticlesystem.SetActive(false);
+        InvokeRepeating("HelpChecker", 1, 1);
+
         brakeAway = false;
         hasGameStarted = false;
-        Time.timeScale = 0.5f;
+
+        // Deactivate initial objects
+        UmbilicalParticlesystem.SetActive(false);
         HelpingHeand.SetActive(false);
         helpingText.SetActive(false);
-
-        InvokeRepeating("HelpChecker", 1, 1);
     }
 
     public void Update()
     {
-        velocity = rb.velocity.magnitude;
-
-        //Debug.Log(velocity);
-        //Debug.Log(speed);
-        Debug.Log(helpTimer);
+        velocity = rb.velocity.magnitude; // Update current velocity
 
         if (hasGameStarted)
-        helpTimer += Time.deltaTime;
+            helpTimer += Time.deltaTime; // Increment help timer if the game has started
 
+        // Check if the object should break away
         if (velocity > 10f && !brakeAway && hasGameStarted)
         {
-            UmbilicalParticlesystem.SetActive(true);
+            UmbilicalParticlesystem.SetActive(true); // Activate umbilical particle system
+
             Debug.Log("Disconnect called");
             brakeAway = true;
-            transform.SetParent(null);
-            Destroy(joint);
+            transform.SetParent(null); // Detach from parent
+            Destroy(joint); // Destroy the joint
+
             rb.drag = 0f;
             rb.freezeRotation = false;
-            pointpointmove.StartCoroutine(pointpointmove.StartMoveDown());
+
+            pointpointmove.StartCoroutine(pointpointmove.StartMoveDown()); // Start move down coroutine
         }
         else if (!brakeAway && hasGameStarted && speed < 10f && velocity > 4f)
         {
-            speed += 0.004f;
+            speed += 0.004f; // Gradually increase speed
         }
         else if (!brakeAway && hasGameStarted && speed < 10f)
-            speed += 0.0005f;
+            speed += 0.0005f; // Gradually increase speed
 
-            if (hasGameStarted && !brakeAway)
-            {
-                Time.timeScale = 0.7f;
-                float moveHorizontal = Input.GetAxis("Horizontal");
-                float moveVertical = Input.GetAxis("Vertical");
-                if(!brakeAway)
-                {
-                    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-                    rb.AddForce(movement * speed);
-                    rb.AddForce(Vector3.down * 15f);
-                }
-                else
-                {
-                    moveHorizontal = 0;
-                    moveVertical = 0;
-                }
-            }
-    }
-    void ChangeDirection()
-    {
+        // Apply movement and forces if the game has started and object hasn't broken away
         if (hasGameStarted && !brakeAway)
         {
-            float randomDirectionX = UnityEngine.Random.Range(-1f, 1f);
-            Vector3 randomDirection = new Vector3(randomDirectionX, 0f, 0f).normalized;
-            rb.velocity = randomDirection * speed / 3;
+            Time.timeScale = 0.7f; // Adjust game time scale
+            float moveHorizontal = Input.GetAxis("Horizontal"); // Get horizontal input
+            float moveVertical = Input.GetAxis("Vertical"); // Get vertical input
+
+            if (!brakeAway)
+            {
+                Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // Create movement vector
+
+                rb.AddForce(movement * speed); // Apply movement force
+                rb.AddForce(Vector3.down * 15f); // Apply downward force
+            }
+            else
+            {
+                moveHorizontal = 0;
+                moveVertical = 0;
+            }
         }
     }
+
+    void ChangeDirection()
+    {
+        // Change direction at intervals if the game has started and object hasn't broken away
+        if (hasGameStarted && !brakeAway)
+        {
+            float randomDirectionX = UnityEngine.Random.Range(-1f, 1f); // Generate random direction
+            Vector3 randomDirection = new Vector3(randomDirectionX, 0f, 0f).normalized; // Normalize direction
+            rb.velocity = randomDirection * speed / 3; // Set new velocity
+        }
+    }
+
     void HelpChecker()
     {
+        // Check if help is needed based on conditions
         if (hasGameStarted && !brakeAway && velocity < 5f && helpTimer > 15f && canRedo)
         {
-            HelpingHeand.SetActive(true);
-            helpingText.SetActive(true);
+            HelpingHeand.SetActive(true); // Activate helping hand
+            helpingText.SetActive(true); // Activate helping text
             if (velocity > 5)
-                canRedo = false;
+                canRedo = false; // Disable redo if velocity is above 5
         }
         else
         {
-            HelpingHeand.SetActive(false);
-            helpingText.SetActive(false);
+            HelpingHeand.SetActive(false); // Deactivate helping hand
+            helpingText.SetActive(false); // Deactivate helping text
         }
     }
 }
