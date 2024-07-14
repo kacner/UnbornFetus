422c088e3dd6975ccdae612faa670259012fe61d
Ejnar40 <Einar.lundqvist@gmail.com>
2024-07-14 16:38:48 +0200

    juiji

--- UnbornFetus/Assets/Code/PlayerCode/PlayerCam.cs
+++ UnbornFetus/Assets/Code/PlayerCode/PlayerCam.cs
@@ -37,10 +37,6 @@ public class PlayerCam : MonoBehaviour
     private GameObject player; // Reference to the player object
     private ParticleSystem particleSystem; // Particle system for speed lines
 
-    /*public bool FirstPerson = false; //Bool deciding if the player is in first person or third person
-    public Transform FirstPersonCamPos;
-    public Transform ThirdPersonCamPos;*/
-
     void Start()
     {
         // Hide Cursor
@@ -80,18 +76,6 @@ public class PlayerCam : MonoBehaviour
         
 
         Speedlines(currentVelocity); // Update speed lines effect
-
-       /* if (Input.GetKeyDown(KeyCode.V) && FirstPerson == false)
-        {
-            FirstPerson = true;
-            cameraPos = FirstPersonCamPos;
-        }
-        else
-        {
-            FirstPerson = false;
-            cameraPos = ThirdPersonCamPos;
-        }*/
-
     }
 
     void PlayerCamra()
